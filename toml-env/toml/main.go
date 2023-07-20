ba761a36f11c264a723fdf5dd251b679a3de5e28
Amarjit Kumar <54772122+Amarjit0511@users.noreply.github.com>
2023-07-20 18:25:54 +0530

    Add files via upload

---
+++ toml-env/toml/main.go
@@ -0,0 +1,51 @@
+package main 
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+// Using viper package to read .env file/files
+// The aim is to return the value of the key
+
+func viperTomlVariable(key string) string {
+	// SetConfigFile explicitly defines the path, name and extension of the config file
+	// viper will use this and not check any of the config paths
+	// .env - It will search for the .env file in the current directory 
+
+	// name of the config file without extension
+	viper.SetConfigFile("config.toml")
+	// Find and read the config file
+	viper.AddConfigPath(".")
+
+	// Finding and reading the config file 
+	err := viper.ReadInConfig()
+
+
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	// viper.Get() returns an empty interface{}
+	// to get the underlying type of the key,
+	// we have to do the type asertion, we know the underlying value is string
+	// if we typ assert to other type it will throw an error
+
+	value, ok := viper.Get(key).(string)
+
+	// If the type is a string then ok will be true 
+	// ok will make sure the program not breaks
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+
+	return value 
+}
+
+func main() {
+	vipertoml := viperTomlVariable("name")
+
+	fmt.Printf("viper ; %s = %s \n", "name", vipertoml)
+}
