a2fd206219ec312650fde275818814583e07a56f
Amarjit Kumar <54772122+Amarjit0511@users.noreply.github.com>
2023-07-22 15:47:09 +0530

    Add files via upload

--- jwt/main.go
+++ jwt/main.go
@@ -20,16 +20,48 @@ type Claims struct {
 	jwt.StandardClaims
 }
 
+// In-memory user storage
+var users = map[string]User{
+	"user1": User{Username: "user1", Password: "password1"},
+	"user2": User{Username: "user2", Password: "password2"},
+	// Add more users as needed.
+}
+
 func main() {
 	r := gin.Default()
 
-	// Unprotected route for user login
+	// User registration endpoint
+	r.POST("/register", register)
+
+	// User login endpoint
 	r.POST("/login", login)
 
 	// Protected route example
 	r.GET("/protected", authMiddleware(), protected)
 
 	r.Run(":8080")
+}
+
+func register(c *gin.Context) {
+	var user User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	// Check if the username already exists
+	if _, exists := users[user.Username]; exists {
+		c.JSON(http.StatusConflict, gin.H{"error": "Username already exists"})
+		return
+	}
+
+	// Replace this with proper password hashing in production.
+	users[user.Username] = User{
+		Username: user.Username,
+		Password: user.Password,
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
 }
 
 func login(c *gin.Context) {
@@ -39,9 +71,20 @@ func login(c *gin.Context) {
 		return
 	}
 
-	// TODO: Implement your own authentication logic here (e.g., check the username and password against a database).
+	// Check if the username exists in the user map.
+	storedUser, exists := users[user.Username]
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
 
-	// For simplicity, let's assume the login is successful for any user.
+	// Replace this with proper password hashing and comparison in production.
+	if user.Password != storedUser.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	// For a successful login, generate a JWT token and send it in the response.
 	token, err := generateToken(user.Username)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
@@ -99,6 +142,8 @@ func authMiddleware() gin.HandlerFunc {
 }
 
 func protected(c *gin.Context) {
+	// Extract the username from the context.
 	username, _ := c.Get("username")
+
 	c.JSON(http.StatusOK, gin.H{"message": "Hello, " + username.(string) + "! This is a protected route."})
 }
